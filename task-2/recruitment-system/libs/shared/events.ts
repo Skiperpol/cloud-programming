008de0daeff1dfe0b03f2c2ac6d12831c605ca27
Dawid <skiperpol1@gmail.com>
2026-04-08 18:31:28 +0200

    feat: Add event constants for recruitment system workflow

---
+++ task-2/recruitment-system/libs/shared/events.ts
@@ -0,0 +1,4 @@
+export const APPLICATION_SUBMITTED = 'ApplicationSubmitted';
+export const DECISION_MADE = 'DecisionMade';
+export const SKILLS_READY = 'SkillsReady';
+export const SAFETY_VERIFIED = 'SafetyVerified';
