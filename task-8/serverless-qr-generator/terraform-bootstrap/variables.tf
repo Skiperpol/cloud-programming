9e19f0af0685f39fe094fbba111b6c29e261b2e4
Dawid <skiperpol1@gmail.com>
2026-05-11 10:54:57 +0200

    feat: Add Terraform bootstrap workflow for serverless QR generator, including S3 bucket and DynamoDB table setup for state management

---
+++ task-8/serverless-qr-generator/terraform-bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region for backend resources"
+}
+
+variable "state_bucket_name" {
+  type        = string
+  description = "S3 bucket name for Terraform state"
+}
+
+variable "lock_table_name" {
+  type        = string
+  description = "DynamoDB table name for Terraform state locking"
+}
