5c797d4d75e09d3464196db07dfb2b5654b6fa54
Dawid <skiperpol1@gmail.com>
2026-05-11 11:16:18 +0200

    refactor: Remove DynamoDB lock table configuration from Terraform workflows and related files, simplifying state management for serverless QR generator

--- task-8/serverless-qr-generator/terraform-bootstrap/main.tf
+++ task-8/serverless-qr-generator/terraform-bootstrap/main.tf
@@ -28,14 +28,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
     }
   }
 }
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- task-8/serverless-qr-generator/terraform-bootstrap/variables.tf
+++ task-8/serverless-qr-generator/terraform-bootstrap/variables.tf
@@ -7,8 +7,3 @@ variable "state_bucket_name" {
   type        = string
   description = "S3 bucket name for Terraform state"
 }
-
-variable "lock_table_name" {
-  type        = string
-  description = "DynamoDB table name for Terraform state locking"
-}
